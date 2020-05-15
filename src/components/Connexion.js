12d66c45e0e58186bcbd9c927c400372f2dc1ce6
dani koskas <koskas.dani@gmail.com>
2020-05-15 18:15:43 +0200

    adding css and Route

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 
 export default class Connexion extends Component {
+  state = {
+    peseudo: "",
+  };
+  handelChange = (event) => {
+    const pseudo = event.taget.value;
+    this.setState({ pseudo });
+  };
   render() {
     return (
       <div className="conenxionBox">
         <form className="connexion">
-          <input placeholder="Username" type="text" required></input>
+          <input
+            value={this.state.peseudo}
+            onChange={this.handelChange}
+            placeholder="Username"
+            type="text"
+            required
+          ></input>
           <button type="submit">GO</button>
         </form>
       </div>
